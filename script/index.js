985ce713ece5b8c454a0817b558d33792bab6c01
LeurisCastillo <leurisa.delossantos@gmail.com>
2021-06-15 14:36:22 -0400

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,34 @@
+const buttonGet = document.getElementById('button-get')
+const buttonPost = document.getElementById('button-post')
+
+buttonGet.addEventListener('click', () => {
+    fetch('http://www.raydelto.org/agenda.php').then(data => data.json()).then(data => {
+
+        const list = document.getElementById('list')
+        for (const userInfo of data) {
+            const listItem = document.createElement('li')
+            listItem.textContent = `${userInfo.nombre} - ${userInfo.apellido} - ${userInfo.telefono}`
+            list.appendChild(listItem)
+        }
+    })
+})
+
+buttonPost.addEventListener('click', () => {
+    const name = document.getElementById('name')
+    const lastName = document.getElementById('lastName')
+    const phone = document.getElementById('phone')
+
+    const newContact = {
+        nombre: name.value,
+        apellido: lastName.value,
+        telefono: phone.value,
+    }
+
+    fetch('http://www.raydelto.org/agenda.php', {
+        method: 'POST',
+        body: JSON.stringify(newContact),
+        headers: {
+            "content-type": "text/plain"
+        }
+    }).then(res => res.json()).then(res => console.log(res))
+})
